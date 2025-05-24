8bb07777b0c37e535227fd9c8555c152acfa2bdc
itpohgero <itpohgero@gmail.com>
2025-05-24 08:47:02 +0700

    Feat: Prisma and better auth

---
+++ app/lib/auth.ts
@@ -0,0 +1,13 @@
+import prisma from "@/server/pkg/prisma-client";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+    emailAndPassword: {
+        enabled: true
+    },
+
+});
--- app/server/pkg/hono.ts
+++ app/server/pkg/hono.ts
@@ -2,10 +2,12 @@ import { trpcServer } from "@hono/trpc-server";
 import { Hono } from "hono";
 import { ContextData } from "../context/main";
 import { appRouter } from "../server-router";
+import { auth } from "@/lib/auth";
 
 export function HonoApp() {
 	const app = new Hono();
 	app.get("/.well-known/*", (c) => c.text("Not found", 404));
+	app.on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw));
 	app.use(
 		"/trpc/*",
 		trpcServer({
