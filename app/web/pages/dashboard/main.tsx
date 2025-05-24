ff4a826da31ecc0b4ef65c3d1ab1fac8a1505003
itpohgero <itpohgero@gmail.com>
2025-05-24 09:17:50 +0700

    fix: sign out

--- app/web/pages/dashboard/main.tsx
+++ app/web/pages/dashboard/main.tsx
@@ -1,7 +1,26 @@
+import { authClient } from "@/lib/better-auth.client";
+import { Button } from "@/web/components/ui/button";
+import { useNavigate } from "react-router";
+
 export default function WelcomePage() {
+	const navigate = useNavigate();
+	const handleSignOut = async () => {
+		
+		// Sign out logic here
+		await authClient.signOut({
+			fetchOptions: {
+				onSuccess: () => {
+					navigate("/");
+				},
+			},
+		});
+	}
 	return (
 		<div className="container mx-auto p-4">
-			<p>Welcome</p>
+			<p>Dashboard</p>
+			<Button variant="outline" onClick={handleSignOut}>
+				handleSignOut
+			</Button>
 		</div>
 	);
 }
