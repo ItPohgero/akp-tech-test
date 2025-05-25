4c02dd11d3071593df64afc78121427822183dd7
itpohgero <itpohgero@gmail.com>
2025-05-25 16:27:28 +0700

    VITE_TRPC_URL

--- app/server/pkg/trpc-client.ts
+++ app/server/pkg/trpc-client.ts
@@ -2,10 +2,12 @@ import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "../server-router";
 
 const port = import.meta.env.VITE_API_PORT || 5173;
+const url = import.meta.env.VITE_TRPC_URL || `http://localhost:${port}/trpc`;
+
 const trpc = createTRPCClient<AppRouter>({
 	links: [
 		httpBatchLink({
-			url: process.env.TRPC_URL || `http://localhost:${port}/trpc`,
+			url
 		}),
 	],
 });
