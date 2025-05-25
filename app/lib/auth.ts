73b9fe197d65a37a2f8965788b64bef6ae012622
itpohgero <itpohgero@gmail.com>
2025-05-25 16:10:36 +0700

    up

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,22 +1,12 @@
-import { betterAuth } from "better-auth"
+import prisma from "@/server/pkg/prisma-client";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
 
 export const auth = betterAuth({
-	baseURL: process.env.BETTER_AUTH_URL || process.env.RAILWAY_PUBLIC_DOMAIN
-		? `https://${process.env.RAILWAY_PUBLIC_DOMAIN}`
-		: "http://localhost:5173",
-
-
-	secret: process.env.BETTER_AUTH_SECRET,
-
-	database: {
+	database: prismaAdapter(prisma, {
 		provider: "postgresql",
-		url: process.env.DATABASE_URL,
+	}),
+	emailAndPassword: {
+		enabled: true,
 	},
-
-	trustedOrigins: [
-		"http://localhost:3000",
-		"http://localhost:5173",
-		"https://your-app-name.up.railway.app",
-		process.env.RAILWAY_PUBLIC_DOMAIN ? `https://${process.env.RAILWAY_PUBLIC_DOMAIN}` : "",
-	].filter(Boolean),
-})
+});
