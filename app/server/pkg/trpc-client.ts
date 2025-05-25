808cfd682f42b65fe64ba48bb756c62fd72fd930
itpohgero <itpohgero@gmail.com>
2025-05-25 16:17:20 +0700

    TRPC_URL

--- app/server/pkg/trpc-client.ts
+++ app/server/pkg/trpc-client.ts
@@ -1,13 +1,11 @@
 import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "../server-router";
 
-const NODE_ENV = process.env.NODE_ENV;
-const PORT = process.env.PORT;
-const RAILWAY_PUBLIC_DOMAIN = process.env.RAILWAY_PUBLIC_DOMAIN
+const port = import.meta.env.VITE_API_PORT || 5173;
 const trpc = createTRPCClient<AppRouter>({
 	links: [
 		httpBatchLink({
-			url: NODE_ENV === "production" ? `${RAILWAY_PUBLIC_DOMAIN}/trpc` : `http://localhost:${PORT}/trpc`,
+			url: process.env.TRPC_URL || `http://localhost:${port}/trpc`,
 		}),
 	],
 });
