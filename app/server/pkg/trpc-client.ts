2ac6f47c585b93f6bc8a8a62e07368d8fa2e9acc
itpohgero <itpohgero@gmail.com>
2025-05-25 16:35:51 +0700

    up

--- app/server/pkg/trpc-client.ts
+++ app/server/pkg/trpc-client.ts
@@ -1,13 +1,10 @@
 import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "../server-router";
 
-// const port = import.meta.env.VITE_API_PORT || 5173;
-// const url = import.meta.env.VITE_TRPC_URL || `http://localhost:${port}/trpc`;
-
 const trpc = createTRPCClient<AppRouter>({
 	links: [
 		httpBatchLink({
-			url: "https://akpstore.itpohgero.com/trpc",
+			url: process.env.TRPC_BASE_URL || "https://akpstore.itpohgero.com/trpc",
 		}),
 	],
 });
